85bd2b1d282ed4cd84dbe532cc176bda389d43c9
KJ Nam <paekma09@gmail.com>
2023-04-22 17:55:12 +0900

    OX quiz_First

---
+++ Pro_Level0_OX quiz/src/com/main/Main.java
@@ -0,0 +1,66 @@
+package com.main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	/*
+	[문제 설명]
+	덧셈, 뺄셈 수식들이 'X [연산자] Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다.
+수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+	[제한사항]
+	연산 기호와 숫자 사이는 항상 하나의 공백이 존재합니다. 
+	단 음수를 표시하는 마이너스 기호와 숫자 사이에는 공백이 존재하지 않습니다.
+	1 ≤ quiz의 길이 ≤ 10
+	X, Y, Z는 각각 0부터 9까지 숫자로 이루어진 정수를 의미하며, 
+	각 숫자의 맨 앞에 마이너스 기호가 하나 있을 수 있고 이는 음수를 의미합니다.
+	X, Y, Z는 0을 제외하고는 0으로 시작하지 않습니다.
+	-10,000 ≤ X, Y ≤ 10,000
+	-20,000 ≤ Z ≤ 20,000
+	[연산자]는 + 와 - 중 하나입니다.
+	[입출력 예]
+	quiz																					result
+	["3 - 4 = -3", "5 + 6 = 11"]												["X", "O"]
+	["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"]	["O", "O", "X", "O"]
+	[입출력 예 설명]
+	입출력 예 #1
+		3 - 4 = -3 은 틀린 수식이므로 "X", 5 + 6 = 11 은 옳은 수식이므로 "O" 입니다. 따라서 ["X", "O"]를 return합니다.
+	입출력 예 #2
+		19 - 6 = 13 은 옳은 수식이므로 "O", 5 + 66 = 71 은 옳은 수식이므로 "O", 5 - 15 = 63 은 틀린 수식이므로 "X", 3 - 1 = 2는 옳은 수식이므로 "O" 따라서 ["O", "O", "X", "O"]를 return합니다.
+	*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int length = sc.nextInt();
+		String[] quiz = new String[length];
+		for(int i=0; i<length; i++) {
+			quiz[i] = sc.next();
+		}
+		
+		String[] answer = new String[quiz.length];
+		
+		// 이 문제의 키포인트는 주어진 quiz문자열 배열을 split을 이용하여 각각의 배열로 만들어
+		// [0],[2],[4]로 [1]부호로 연산하여 결과 값이 true이면 "O" , 아니면 "Y" 를 answer 배열에 담으면 된다.
+        for(int i=0; i<quiz.length; i++){
+            String[] temp = quiz[i].split(" ");
+            
+            int numX = Integer.parseInt(temp[0]);
+            int numY = Integer.parseInt(temp[2]);
+            int numZ = Integer.parseInt(temp[4]);
+            
+            if(temp[1].equals("+")){
+                if(numX+numY == numZ){
+                    answer[i] = "O";
+                } else{
+                    answer[i] = "X";
+                }
+            } else{
+                if(numX-numY == numZ){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }
+        }
+        System.out.println(Arrays.toString(answer));
+	}
+}
