90cfe3684ccd94042b9ae2d51d55ac48ee9775a1
KJ Nam <paekma09@gmail.com>
2023-04-26 03:01:39 +0900

    Delete an element from an array_First

---
+++ Pro_Level0_Delete an element from an array/src/com/main/Main.java
@@ -0,0 +1,61 @@
+package com.main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+	/*
+	[문제 설명]
+	정수 배열 arr과 delete_list가 있습니다. 
+	arr의 원소 중 delete_list의 원소를 모두 삭제하고 
+	남은 원소들은 기존의 arr에 있던 순서를 유지한 배열을 return 하는 solution 함수를 작성해 주세요.
+	[제한사항]
+	1 ≤ arr의 길이 ≤ 100
+	1 ≤ arr의 원소 ≤ 1,000
+	arr의 원소는 모두 서로 다릅니다.
+	1 ≤ delete_list의 길이 ≤ 100
+	1 ≤ delete_list의 원소 ≤ 1,000
+	delete_list의 원소는 모두 서로 다릅니다.
+	[입출력 예]
+	arr										delete_list								result
+	[293, 1000, 395, 678, 94]		[94, 777, 104, 1000, 1, 12]	[293, 395, 678]
+	[110, 66, 439, 785, 1]			[377, 823, 119, 43]				[110, 66, 439, 785, 1]
+	[입출력 예 설명]
+	입출력 예 #1
+		예제 1번의 arr의 원소 중 1000과 94가 delete_list에 있으므로 이 두 원소를 삭제한 
+		[293, 395, 678]을 return 합니다.
+	입출력 예 #2
+		예제 2번의 arr의 원소 중 delete_list에 있는 원소는 없습니다. 따라서 arr 그대로인 
+		[110, 66, 439, 785, 1]을 return 합니다.
+	*/
+	public static void main(String[] args) {
+		int[] arr1 = {293, 1000, 395, 678, 94};
+		int[] delete_list1 = {94, 777, 104, 1000, 1, 12};
+		int[] arr2 = {110, 66, 439, 785, 1};
+		int[] delete_list2 = {377, 823, 119, 43};
+		System.out.print(Arrays.toString(arr1)+" ");
+		System.out.print(Arrays.toString(delete_list1)+" ");
+		System.out.println(Arrays.toString(solution(arr1, delete_list1)));
+		System.out.print(Arrays.toString(arr2)+" ");
+		System.out.print(Arrays.toString(delete_list2)+" ");
+		System.out.println(Arrays.toString(solution(arr2, delete_list2)));
+	}
+	public static int[] solution(int[] arr, int[] delete_list) {
+		// list 생성
+		List<Integer> list = new ArrayList<>(arr.length);
+		// arr에 있는 요소 list에 저장 (향상된 for 문)
+		for(int i : arr) {
+			list.add(i);
+		}
+		// delete_list에 있는 요소값 list에 있는지 여부 확인하여 존재하면 list에서 해당 요소 전체 삭제
+		for(int i=0; i<delete_list.length;i++) {
+			if(list.contains(delete_list[i])) {
+				while(list.remove(Integer.valueOf(delete_list[i]))) { }
+			}
+		}
+		// stream 이용하여 list를 int배열로 변환 저장 (매우 중요)
+		int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
